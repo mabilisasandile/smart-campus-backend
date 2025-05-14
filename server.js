e9cb9af1a531224e79b64f1759a2310c8e0cfc6f
mabilisasandile <mabilisasandile@gmail.com>
2025-05-15 05:51:15 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import userRouter from "./routes/userRoute.js";
+
+
+// App Config
+const app = express();
+const port = process.env.port || 4000;
+connectDB();
+
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+// app.use(cors({ 
+//   origin: [
+//   "http://localhost:5173"
+//   ] 
+// }));
+
+
+// api endpoints
+app.use("/api/user", userRouter);
+
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
+app.listen(port, () => console.log("Server started on PORT: " + port));
